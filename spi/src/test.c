8c6d34f7f7e07a519c27a0e7b24e3fe64ddb9b34
Ricardo Cañuelo Navarro <ricardo.canuelo@gmail.com>
2011-05-20 10:45:16 +0200

    Example spi/src/test.c documented and slightly modified

--- spi/src/test.c
+++ spi/src/test.c
@@ -1,3 +1,32 @@
+/*
+ * Copyright (c) Ricardo Cañuelo Navarro
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
 #include <mc1322x.h>
 #include <redbee-econotag.h>
 #include <config.h>
@@ -12,21 +41,26 @@ int main(void)
 {
 	spiErr_t error;
 	spiConfig_t config;
-	uint32_t data[16];
+	uint32_t data;
 	uint32_t i;
 
+	/* Initialize RTC */
 	rtc_init_osc(0);
 	rtc_calibrate();
 
+	/* Configure SPI pins to Functional 1
+	 * NOTE: GPIOs 0 to 15 except 11 (TMR3) are set in Functional 1 here */
 	*GPIO_FUNC_SEL0 = 0x55155555;
+
+	/* Set GPIO11 (TMR3) to output and HIGH */
 	*GPIO_PAD_DIR0 |= 1 << 11;
 	*GPIO_DATA_SET1 |= 1 << 11;
-	memset(data, 0, 16 * sizeof(uint32_t));
-	data[0] = 0x12345678;
-	data[2] = 0x80000000;
-	rtc_delay_ms(100);
+
+	/* Output data via SPI ten times with one second pauses in between
+	 * GPIO11 (TMR3) is used as an additional Slave Select */
+	data = 0x12345678;
 	error = SPI_Open();
-//	while (1) {
+	for (i = 0; i < 10; ++i) {
 		*GPIO_DATA_RESET1 |= 1 << 11;
 		rtc_delay_ms(2);
 		config.ClkCtrl.Word = 0;
@@ -34,17 +68,12 @@ int main(void)
 		config.ClkCtrl.Bits.DataCount = 32;
 		config.ClkCtrl.Bits.ClockCount = 32;
 		config.Setup.Bits.ClockFreq = 7;
-		config.Setup.Bits.SsDelay = 0;
-		config.Setup.Bits.MisoPhase = 0;
 		config.Setup.Bits.ClockPhase = 1;
-		config.Setup.Bits.ClockPol = 0;
 		error = SPI_SetConfig(&config);
-		error = SPI_WriteSync(data[0]);
-	//	for (i = 0; i < 16; ++i)
-	//		error = SPI_WriteSync(data[i]);
+		error = SPI_WriteSync(data);
 		*GPIO_DATA_SET1 |= 1 << 11;
 		rtc_delay_ms(1000);
-//	}
+	}
 	error = SPI_Close();
 	while (1) ;
 }
